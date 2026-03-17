38ee647a0d3834d855ec46a89ce640687be2a890
vidx-paiva <deyviddepaiva@gmail.com>
2026-03-17 19:36:20 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,6 +1,14 @@
 <?php
 include 'config.php';
 $dados = json_decode(file_get_contents("php://input"), true);
+
 $maquina = $dados["maquina"];
 $status = $dados["status"];
+
+$log = "Máquina: $maquina | Status: $status";
+
+
+file_put_contents("log.txt", $log . PHP_EOL, FILE_APPEND);
+
+echo "OK";
 ?>
